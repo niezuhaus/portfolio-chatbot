19aaedc9780cc6cdd9e163136e59bceb08c840fa
niezuhaus <a@niezuhaus.de>
2024-05-19 21:41:14 +0200

    first api route

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post('/talk', (req, res) => {
+  console.log("received message: " + req)
+    res.json({answer: 'yes'});
+  });  
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
